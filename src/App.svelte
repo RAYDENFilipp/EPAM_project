1a87fa4a1dd71e86ac66a252c773800ea74aa964
Philipp Lypniakov <filipprayden@gmail.com>
2019-07-26 00:29:39 +0300

    Removed redundant verbiage

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,7 @@
 <script>
   import Navbar from "./components/Navbar/Navbar.svelte";
   import Imageslider from "./components/Imageslider/Imageslider.svelte";
-  export let name;
 </script>
-
-<style>
-  h1 {
-    color: purple;
-  }
-</style>
 
 <Navbar />
 <Imageslider />
-
-<h1>Hello {name}!</h1>
