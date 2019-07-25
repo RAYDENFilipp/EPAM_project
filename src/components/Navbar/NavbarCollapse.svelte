95b29b9dd079e4d4367e7fb73baa3b288c1b70c7
Philipp Lypniakov <filipprayden@gmail.com>
2019-07-25 23:07:23 +0300

    Created NavBarCollapse

--- src/components/Navbar/NavbarCollapse.svelte
+++ src/components/Navbar/NavbarCollapse.svelte
@@ -2,20 +2,30 @@
   export let collapsed = true;
 </script>
 
-  <div class:collapse={collapsed} class="navbar-collapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline mt-2 mt-md-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
+<div class:collapse={collapsed} class="navbar-collapse">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="#home">
+        Home
+        <span class="sr-only">(current)</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#blog">Blog</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#contact">Contacts</a>
+    </li>
+  </ul>
+  <form class="form-inline mt-2 mt-md-0">
+    <input
+      class="form-control mr-sm-2"
+      type="text"
+      placeholder="Search"
+      aria-label="Search" />
+    <input
+      class="btn btn-outline-success my-2 my-sm-0"
+      type="submit"
+      value="Search" />
+  </form>
+</div>
