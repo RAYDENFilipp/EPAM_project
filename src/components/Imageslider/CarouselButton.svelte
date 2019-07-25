b0554f29c711f974603fd054d4a3c3d45a03d1cd
Philipp Lypniakov <filipprayden@gmail.com>
2019-07-25 23:07:56 +0300

    Created Carousel Button

---
+++ src/components/Imageslider/CarouselButton.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let direction;
+  export let screenReaderValue;
+</script>
+
+<style>
+  button {
+    background: transparent;
+    outline: none;
+    text-decoration: none;
+  }
+
+  button:focus {
+    opacity: 0.5;
+  }
+</style>
+
+<button class="carousel-control-{direction} border-0" on:click>
+  <span class="carousel-control-{direction}-icon" aria-hidden="true" />
+  <span class="sr-only">{screenReaderValue}</span>
+</button>
