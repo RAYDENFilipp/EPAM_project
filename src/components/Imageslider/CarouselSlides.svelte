2e42e7efe1e2c8ea2ae405a0fc916d119b05dba2
Philipp Lypniakov <filipprayden@gmail.com>
2019-07-25 23:07:45 +0300

    Created CarouselSlides

---
+++ src/components/Imageslider/CarouselSlides.svelte
@@ -0,0 +1,64 @@
+<script>
+  import { slideIndex } from "./stores.js";
+  import { fade } from "svelte/transition";
+
+  let images = [
+    {
+      id: 0,
+      src: "./media/img_lights_wide.jpg",
+      caption: "First title",
+      color: "text-light"
+    },
+    {
+      id: 1,
+      src: "./media/img_mountains_wide.jpg",
+      caption: "Second Title",
+      color: "text-light"
+    },
+    {
+      id: 2,
+      src: "./media/img_nature_wide.jpg",
+      caption: "Third Title",
+      color: "text-light"
+    }
+  ];
+
+  /** with this 'reactive declaration' we can handle change of store's value
+   * and draw a respective slider
+   * */
+  $: itemtoDraw = images[$slideIndex];
+</script>
+
+<style>
+  .carousel {
+    background: no-repeat center center scroll;
+    background-size: cover;
+    min-height: 250px;
+  }
+
+  .carousel__caption--transparent {
+    max-width: 500px;
+    min-width: 70%;
+    opacity: 0.8;
+  }
+</style>
+
+<div class="carousel-inner">
+
+  {#each images as { id, src, caption, color }}
+    <!-- one of the ways we can render only one slider to the DOM instead of three -->
+    {#if $slideIndex === id}
+      <div
+        class:active={id === $slideIndex}
+        class="carousel carousel-item d-flex flex-column justify-content-center
+        align-items-center"
+        style="background-image: url({src});"
+        transition:fade>
+        <section class="carousel__caption--transparent {color} bg-dark px-2">
+          <h2>{caption}</h2>
+          <p class="h5">This is a paragraph for the {caption}.</p>
+        </section>
+      </div>
+    {/if}
+  {/each}
+</div>
