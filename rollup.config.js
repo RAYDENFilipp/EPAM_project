04b9358745ded732d33fee0a55960c77cf735a26
Philipp Lypniakov <filipprayden@gmail.com>
2019-07-27 18:18:04 +0300

    Remade indentation for project as 2 spaces

--- rollup.config.js
+++ rollup.config.js
@@ -11,87 +11,87 @@ import buble from "rollup-plugin-buble";
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-    input: "src/main.js",
-    output: {
-        sourcemap: true,
-        format: "iife",
-        name: "app",
-        file: "public/bundle.js"
-    },
-    plugins: [
-        svelte({
-            // enable run-time checks when not in production
-            dev: !production,
-            // we'll extract any component CSS out into
-            // a separate file — better for performance
+  input: "src/main.js",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/bundle.js"
+  },
+  plugins: [
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file — better for performance
 
-            emitCss: true,
+      emitCss: true,
 
-            css: css => {
-                css.write("public/bundle.css");
-            }
-        }),
+      css: css => {
+        css.write("public/bundle.css");
+      }
+    }),
 
-        postcss({
-            plugins: [require("autoprefixer")]
-        }),
+    postcss({
+      plugins: [require("autoprefixer")]
+    }),
 
-        url({
-            limit: 10 * 1024, // inline files < 10k, copy files > 10k
-            include: ["**/*.svg", "**/*.jpg"], // defaults to .svg, .png, .jpg and .gif files
-            emitFiles: true, // defaults to true
-            fileName: "[dirname][name][extname]"
-        }),
+    url({
+      limit: 10 * 1024, // inline files < 10k, copy files > 10k
+      include: ["**/*.svg", "**/*.jpg"], // defaults to .svg, .png, .jpg and .gif files
+      emitFiles: true, // defaults to true
+      fileName: "[dirname][name][extname]"
+    }),
 
-        buble({
-            exclude: ["node_modules/@babel/**", "node_modules/core-js/**"]
-        }),
-        // If you have external dependencies installed from
-        // npm, you'll most likely need these plugins. In
-        // some cases you'll need additional configuration —
-        // consult the documentation for details:
-        // https://github.com/rollup/rollup-plugin-commonjs
-        resolve({
-            browser: true,
-            dedupe: importee =>
-                importee === "svelte" || importee.startsWith("svelte/")
-        }),
-        commonjs(),
+    buble({
+      exclude: ["node_modules/@babel/**", "node_modules/core-js/**"]
+    }),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve({
+      browser: true,
+      dedupe: importee =>
+        importee === "svelte" || importee.startsWith("svelte/")
+    }),
+    commonjs(),
 
-        // Watch the `public` directory and refresh the
-        // browser on changes when not in production
-        !production && livereload("public"),
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("public"),
 
-        // compile to good old IE11 compatible ES5
-        // babel({
-        //     extensions: [".js", ".mjs", ".html", ".svelte"],
-        //     runtimeHelpers: true,
-        //     exclude: ["node_modules/@babel/**", "node_modules/core-js/**"],
-        //     presets: [
-        //         [
-        //             "@babel/preset-env",
-        //             {
-        //                 targets: "> 1%",
-        //                 useBuiltIns: "usage",
-        //                 corejs: 3
-        //             }
-        //         ]
-        //     ],
-        //     plugins: [
-        //         "@babel/plugin-syntax-dynamic-import",
-        //         [
-        //             "@babel/plugin-transform-runtime",
-        //             {
-        //                 useESModules: true
-        //             }
-        //         ]
-        //     ]
-        // }),
-        // If we're building for production (npm run build
-        // instead of npm run dev), minify
-        production && terser()
-    ],
-    watch: {
-        clearScreen: false
-    }
+    // compile to good old IE11 compatible ES5
+    // babel({
+    //     extensions: [".js", ".mjs", ".html", ".svelte"],
+    //     runtimeHelpers: true,
+    //     exclude: ["node_modules/@babel/**", "node_modules/core-js/**"],
+    //     presets: [
+    //         [
+    //             "@babel/preset-env",
+    //             {
+    //                 targets: "> 1%",
+    //                 useBuiltIns: "usage",
+    //                 corejs: 3
+    //             }
+    //         ]
+    //     ],
+    //     plugins: [
+    //         "@babel/plugin-syntax-dynamic-import",
+    //         [
+    //             "@babel/plugin-transform-runtime",
+    //             {
+    //                 useESModules: true
+    //             }
+    //         ]
+    //     ]
+    // }),
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
--- src/utilities/stores.js
+++ src/utilities/stores.js
@@ -6,14 +6,13 @@ import { writable } from "svelte/store";
  * i.e. share state between indirectly related elements
  */
 function createslideIndexStore() {
-    const { subscribe, update, set } = writable(0);
-
-    return {
-        subscribe,
-        increment: () => update(n => (n === 2 ? 0 : ++n)),
-        decrement: () => update(n => (n === 0 ? 2 : --n)),
-        set
-    };
+  const { subscribe, update, set } = writable(0);
+  return {
+    subscribe,
+    increment: () => update(n => (n === 2 ? 0 : ++n)),
+    decrement: () => update(n => (n === 0 ? 2 : --n)),
+    set
+  };
 }
 
 export const slideIndex = createslideIndexStore();
