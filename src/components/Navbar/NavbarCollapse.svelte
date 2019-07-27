9cc57956bc001d3a6ae5757b9dca89a8f79be378
Philipp Lypniakov <filipprayden@gmail.com>
2019-07-27 18:17:45 +0300

    Applied Smooth Scroll and buttons highlight

--- src/components/Navbar/NavbarCollapse.svelte
+++ src/components/Navbar/NavbarCollapse.svelte
@@ -1,23 +1,58 @@
 <script>
+  import SmoothScroll from "smooth-scroll";
   export let collapsed = true;
+
+  // animation polifill for old Browsers
+
+  let links = [
+    { active: true, href: "#home", text: "Home" },
+    { active: false, href: "#blog", text: "Blog" },
+    { active: false, href: "#contacts", text: "Contacts" }
+  ];
+
+    let scroll = new SmoothScroll('a[href*="#"]', {
+      speed: 600,
+      speedAsDuration: true
+    });
+
+  function handleClick(active, href) {
+    if (!active) {
+      links = links.map(link => {
+        link.active =
+          link.href === href || link.active ? !link.active : link.active;
+        return link;
+      });
+    }
+  }
 </script>
+
+<style>
+  @media (min-width: 768px) {
+    li {
+      border-right: 0.2rem solid;
+      min-width: 5rem;
+    }
+
+    li:first-child {
+      border-left: 0.2rem solid;
+    }
+  }
+</style>
 
 <div class:collapse={collapsed} class="navbar-collapse">
   <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link" href="#home">
-        Home
-        <span class="sr-only">(current)</span>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#blog">Blog</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#contacts">Contacts</a>
-    </li>
+    {#each links as { active, href, text }}
+      <li class:active class="nav-item border-light">
+        <a class="nav-link" {href} on:click={() => handleClick(active, href)}>
+          {text}
+          {#if active}
+            <span class="sr-only">(current)</span>
+          {/if}
+        </a>
+      </li>
+    {/each}
   </ul>
-  <form class="form-inline mt-2 mt-md-0 mr-lg-2">
+  <!-- <form class="form-inline mt-2 mt-md-0 mr-lg-2">
     <input
       class="form-control mr-sm-2"
       type="text"
@@ -27,5 +62,5 @@
       class="btn btn-outline-success my-2 my-sm-0"
       type="submit"
       value="Search" />
-  </form>
+  </form> -->
 </div>
