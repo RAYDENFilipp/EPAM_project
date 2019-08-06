3ea6d2b2f2c3504a611c83d84718c09625ba500c
Philipp Lypniakov <filipprayden@gmail.com>
2019-08-06 05:20:24 +0300

    Contacts section created

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,15 @@
   import Imageslider from "./components/Imageslider/Imageslider.svelte";
   import Main from "./components/Main/Main.svelte";
   import About from "./components/About/About.svelte";
-  import Contacts from "../Contacts/Contacts.svelte";
+  import Contacts from "./components/Contacts/Contacts.svelte";
 </script>
 
 <Navbar />
 {#if $pagePicked === '#about'}
   <About />
-{:else if $pagePicked === '#about'}
+{:else if $pagePicked === '#contacts'}
+  <Contacts />
+{:else}
   <Login />
   <Main />
 {/if}
